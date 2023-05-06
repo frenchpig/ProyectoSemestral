d26128032b51e9c184b84373ddabf50b120870f2
frenchpig <auspont.javier@gmail.com>
2023-05-05 22:47:17 -0600

    eliminacion de comentarios sobrantes

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,22 +1,3 @@
-//Script para validar la contraseña
-/*
-$(document).ready(function() {
-  $('form').submit(function(event) {
-    event.preventDefault();
-    var password = $('#password').val();
-    var confirmPassword = $('#confirm-password').val();
-    if (password !== confirmPassword) {
-      alert('Las contraseñas no coinciden');
-    } else {
-      alert('Contraseña válida');
-      // Aquí puedes realizar cualquier acción adicional que desees
-    }
-  });
-});
-*/
-
-
-
 $(document).ready(function(){
   $("#form-registro").on("submit", function(event){
     event.preventDefault();
